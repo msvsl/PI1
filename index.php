dfc84b1cb0f7ff144e7a83822a2dbd832abd0735
msvsl <musaevmsvsl@gmail.com>
2017-03-17 12:54:21 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+	$a = 10;
+	$b = 20;
+	echo $a." ".$b;
+?>
+<br>
+<?php
+	$c = 10;
+	$d = 20;
+	echo $e = $c+$d;
+?>
+
+<br>
+<?php
+	$f = 10;
+	$g = 20;
+	$i = $c+$d;
+	echo $i*$i*$i;
+?>
+
+<br>
+<?php
+	$j = 10;
+	$h = 20;
+	$l = $c-$d;
+	echo $l;
+?>
+
+<br>
+<?php
+	$m = 'Программа';
+	$n = 'работает';
+	$o = $m." ".$n.' хорошо';
+	echo $o;
+?>
+
+</body>
+</html>
